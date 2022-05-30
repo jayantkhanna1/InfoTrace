628693aea8539d8aa0ad35a04993bb7129a90229
jayantkhanna1 <jayantkhanna3105@gmail.com>
2022-05-30 20:29:28 +0530

    changes

---
+++ handler.py
@@ -0,0 +1,13 @@
+# sherlock
+# email spoof
+# location from image
+# Name search
+# phone number lookup
+# email lookup
+# ip lookup
+# domain lookup
+# url lookup
+# hash lookup
+# social media lookup
+# wifi cracking
+# password cracking md5,sha256,sha512 etc.
