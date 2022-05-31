c29615b3b2fe583a1224fc1fd71ffa0fe495990f
jayantkhanna1 <jayantkhanna3105@gmail.com>
2022-05-31 18:20:30 +0530

    changes

--- handler.py
+++ handler.py
@@ -11,3 +11,18 @@
 # social media lookup
 # wifi cracking
 # password cracking md5,sha256,sha512 etc.
+
+
+
+import subprocess
+name = input()
+command="cd sherlock && python sherlock " +name+" --timeout 1"
+print(command)
+subprocess = subprocess.Popen(
+    command ,shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+)
+
+subprocess_return = subprocess.stdout.read()
+ans=str(subprocess_return).split('\\n')
+for x in ans:
+    print (x)
